435ea3bb36123ab97989b718023dee6b9774a77d
Drew <andrew.engelstein@gmail.com>
2019-01-14 15:11:39 -0800

    Add #bonus and #salary_of_sub_employees to Manager class. Also Add file testing

--- class_inheritance/class_inheritance.rb
+++ class_inheritance/class_inheritance.rb
@@ -1,4 +1,7 @@
 class Employee
+
+  attr_reader :salary
+
   def initialize(name, title, salary, boss)
     @name = name
     @title = title
@@ -16,4 +19,34 @@ class Manager < Employee
     super(name, title, salary, boss)
     @employees = employees
   end
+
+  def bonus(multiplier)
+    bonus = salary_total_of_employees_beneath_you * multiplier
+  end
+
+  def salary_total_of_employees_beneath_you
+    total = 0
+    @employees.each do |employee|
+      if employee.is_a?(Manager)
+        total += employee.salary
+        total += employee.salary_total_of_employees_beneath_you
+      else
+        total += employee.salary
+      end
+    end
+
+    total
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  david = Employee.new("David", "TA", 10_000, "Darren")
+  shawna = Employee.new("Shawna", "TA", 12_000, "Darren")
+  darren = Manager.new("Darren", "TA Manager", 78_000, "Ned", [david, shawna])
+  ned = Manager.new("Ned", "Founder", 1_000_000, nil, [darren])
+
+  p ned.bonus(5)
+  p darren.bonus(4)
+  p david.bonus(3)
+
 end
