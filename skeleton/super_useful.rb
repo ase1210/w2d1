7bf93339fb5d3ae283e90ef71bed1aedfc3d6eef
Drew <andrew.engelstein@gmail.com>
2019-01-14 14:38:49 -0800

    Add phase2 & phase 3

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -1,25 +1,38 @@
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
+rescue ArgumentError
+  nil 
 end
 
 # PHASE 3
+class CoffeeError < StandardError; end
+
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
   else 
-    raise StandardError 
+    raise ArgumentError
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin 
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError
+    retry
+  rescue ArgumentError
+    puts "Ugg.  I can't eat #{maybe_fruit}."
+    nil
+  end
 end  
 
 # PHASE 4
